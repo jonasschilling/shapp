f5d87043ca8b1f228fd9dca21fae08c7d19bf9c1
Jonas Schilling <jonas.schilling@sap.com>
2021-11-25 08:41:52 +0100

    shapp initial

--- src/main/java/com/schilling/shapp/controller/general/UserController.java
+++ src/main/java/com/schilling/shapp/controller/general/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
